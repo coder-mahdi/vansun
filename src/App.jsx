be5374b89b7372852ef46d29c102f7fb9671c920
mahdi-mim <mahdiroozbahani25@gmail.com>
2025-04-14 14:27:02 -0700

    updated booking

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
 import Home from './pages/Home';
 import BookNow from './pages/BookNow';
 import BookingPage from './pages/‌BookingPage';
-import BookingsList from './pages/BookingsList';
+
 
 function App() {
 
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/BookNow" element={<BookNow />} />
         <Route path="/booking/:productId" element={<BookingPage />} />
-        <Route path="/bookings" element={<BookingsList />} />
+
     
       </Routes>
 
