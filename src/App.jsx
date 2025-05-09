994ced267f99d7c978560e41b4f831d1600c5fb6
mahdi-mim <mahdiroozbahani25@gmail.com>
2025-05-08 20:54:23 -0700

    added vit config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import BookNow from './pages/BookNow';
 import BookingPage from './pages/‌BookingPage';
